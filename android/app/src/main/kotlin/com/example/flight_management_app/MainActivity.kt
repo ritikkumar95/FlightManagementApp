659b09e866995ad3c7c4f6d8e6da8789bd0e5805
Ritik Kumar <madaanritik120@gmail.com>
2024-07-23 20:31:26 -0300

    Initial project setup with directory structure and basic files

---
+++ android/app/src/main/kotlin/com/example/flight_management_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flight_management_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'customer_list/customer_list_page.dart';
+import 'airplane_list/airplane_list_page.dart';
+import 'flights_list/flights_list_page.dart';
+import 'reservation/reservation_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flight Management App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+      routes: {
+        '/customerList': (context) => CustomerListPage(),
+        '/airplaneList': (context) => AirplaneListPage(),
+        '/flightsList': (context) => FlightsListPage(),
+        '/reservation': (context) => ReservationPage(),
+      },
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flight Management App'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/customerList');
+              },
+              child: Text('Customer List Page'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/airplaneList');
+              },
+              child: Text('Airplane List Page'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/flightsList');
+              },
+              child: Text('Flights List Page'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/reservation');
+              },
+              child: Text('Reservation Page'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
